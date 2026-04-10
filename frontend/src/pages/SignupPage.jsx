b0d5cbecd4407b6b97bbc25d06b72abcfc722436
Shreyasrp07 <3shreyas2007@gmail.com>
2026-04-10 13:25:47 +0530

    Remove upiId from SignupPage state

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -24,7 +24,6 @@ const SignupPage = () => {
     college: '',
     branch: '',
     yearOfAdmission: '',
-    upiId: '',
     password: prefilledPassword
   });
   const [error, setError] = useState('');
