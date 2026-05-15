3234676d16cda406aae241627576dc813e063c90
Shreyas-patil07 <3shreyas2007@gmail.com>
2026-05-15 23:21:49 +0530

    fix: allow all headers in CORS for multipart upload support

--- backend/app/main.py
+++ backend/app/main.py
@@ -187,7 +187,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization", "Accept", "Origin"],
+    allow_headers=["*"],
     expose_headers=["Content-Length", "Content-Type"],
     max_age=3600,
 )
